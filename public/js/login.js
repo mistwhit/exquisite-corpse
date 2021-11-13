233a9eba253a421fcec56a4cb7522515b153e019
alexdmacon <alexdmacon@gmail.com>
2021-11-13 13:06:54 -0600

    trying to center div on poem page

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/write');
+      // document.location.replace('/write');
     } else {
       alert(response.statusText);
     }
